fb8fc4631d4e00274fa495117985b77633f644f5
olemp <olemp@puzzlepart.com>
2019-01-09 11:23:59 +0100

    Updated ProjectExtensions #28

--- SharePointFramework/ProjectExtensions/src/extensions/projectSetup/tasks/SetupProjectInformation/index.ts
+++ SharePointFramework/ProjectExtensions/src/extensions/projectSetup/tasks/SetupProjectInformation/index.ts
@@ -1,16 +1,22 @@
 import { override } from '@microsoft/decorators';
 import { BaseTask, IBaseTaskParams } from '../BaseTask';
-import { sp } from '@pnp/sp';
+import { Web } from '@pnp/sp';
 import { Logger, LogLevel } from '@pnp/logging';
+import HubSiteService from '../../../../../../services/HubSiteService';
 
 export class SetupProjectInformation extends BaseTask {
     @override
     public async execute(params: IBaseTaskParams) {
         super.execute(params);
         Logger.log({ message: '(ProjectSetupApplicationCustomizer) SetupProjectInformation', level: LogLevel.Info });
-        const list = sp.web.lists.getByTitle('Prosjektegenskaper');
-        const { ListItemEntityTypeFullName } = await list.select('ListItemEntityTypeFullName').get();
-        await list.items.add({ ContentTypeId: '0x0100BE5932D2FEE7174E8638FF16FB5AC913' }, ListItemEntityTypeFullName);
+        const { pageContext } = params.context;
+        const hubSite = await HubSiteService.GetHubSiteById(pageContext.web.absoluteUrl, pageContext.legacyPageContext.hubSiteId);
+        Logger.log({ message: `(ProjectSetupApplicationCustomizer) SetupProjectInformation: Retrieved hub site at ${hubSite.SiteUrl}`, level: LogLevel.Info });
+        const hubSiteRootWeb = new Web(hubSite.SiteUrl);
+        const projectsList = hubSiteRootWeb.lists.getByTitle('Prosjekter');
+        const projectItem = { Title: pageContext.web.title, GtGroupId: pageContext.legacyPageContext.groupId };
+        Logger.log({ message: `(ProjectSetupApplicationCustomizer) SetupProjectInformation: Adding project to list 'Prosjekter' at ${hubSite.SiteUrl}`, data: { projectItem }, level: LogLevel.Info });
+        await projectsList.items.add(projectItem);
     }
 }
 
