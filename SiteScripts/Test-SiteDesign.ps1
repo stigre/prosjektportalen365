391e7f97caceaa00437dd84da1e64d27fe2523d6
olemp <olemp@puzzlepart.com>
2019-01-14 15:31:18 +0100

    SiteScripts #14

--- SiteScripts/Test-SiteDesign.ps1
+++ SiteScripts/Test-SiteDesign.ps1
@@ -4,7 +4,7 @@ Param(
     [Parameter(Mandatory = $false)]
     [string]$Credentials,
     [Parameter(Mandatory = $false)]
-    $SiteUrlTemplate = "Prosjektportalen-O365-{0}",
+    $SiteUrlTemplate = "Prosjektportalen-03-01-{0}",
     [Parameter(Mandatory = $false)]
     $StartIndex = 1,
     [Parameter(Mandatory = $false)]
@@ -40,9 +40,9 @@ $CreatedSiteUrl = $null
 
 while (-not $CreatedSiteUrl) {
     Try {
-        $Title = $SiteUrlTemplate -f $idx.ToString("0000")
+        $Title = $SiteUrlTemplate -f $idx
         Write-Host "[VERBOSE] Attempting to create site $Title"
-        $CreatedSiteUrl = New-PnPSite -Type TeamSite -Title "Adopsjonsutvikling 2020" -Alias $Title -IsPublic:$true -Connection $SiteConnection -ErrorAction Stop 
+        $CreatedSiteUrl = New-PnPSite -Type TeamSite -Title $Title -Alias $Title -IsPublic:$true -Connection $SiteConnection -ErrorAction Stop 
         Write-Host "[INFO] Successfully created site $CreatedSiteUrl"
         Disconnect-PnPOnline -Connection $SiteConnection
     }
@@ -54,23 +54,23 @@ while (-not $CreatedSiteUrl) {
 
 Write-Host "[INFO] Connecting to $CreatedSiteUrl"
 $CreatedSiteConnection = Connect-PnPOnline -Url $CreatedSiteUrl -Credentials $Credentials -ReturnConnection
-Try {
-    if ($Invoke.IsPresent) {
-        Write-Host "[INFO] Invoking Site Design $SiteDesignTitle using Invoke-PnPSiteDesign"
-        Invoke-PnPSiteDesign -Identity $SiteDesignTitle -Connection $CreatedSiteConnection -ErrorAction Stop
-    }
-    else {
-        if (-not [string]::IsNullOrWhiteSpace($AdminSiteUrl)) {
-            Write-Host "[INFO] Connecting to $AdminSiteUrl"
-            Connect-SPOService -Url $AdminSiteUrl -Credential (New-Object System.Management.Automation.PSCredential $UserName, $Password)
-            $SiteDesign = Get-SPOSiteDesign | Where-Object { $_.Title -eq $SiteDesignTitle }
-            Write-Host "[INFO] Invoking Site Design $SiteDesignTitle using Add-SPOSiteDesignTask"
-            Add-SPOSiteDesignTask -SiteDesignId $SiteDesign.Id.Guid -WebUrl $CreatedSiteUrl
-            Start-Process "chrome.exe" $CreatedSiteUrl
-        }
-    }
-}
-Catch [Exception] {
-    Write-Host $_.Exception.Message
-}
+# Try {
+#     if ($Invoke.IsPresent) {
+#         Write-Host "[INFO] Invoking Site Design $SiteDesignTitle using Invoke-PnPSiteDesign"
+#         Invoke-PnPSiteDesign -Identity $SiteDesignTitle -Connection $CreatedSiteConnection -ErrorAction Stop
+#     }
+#     else {
+#         if (-not [string]::IsNullOrWhiteSpace($AdminSiteUrl)) {
+#             Write-Host "[INFO] Connecting to $AdminSiteUrl"
+#             Connect-SPOService -Url $AdminSiteUrl -Credential (New-Object System.Management.Automation.PSCredential $UserName, $Password)
+#             $SiteDesign = Get-SPOSiteDesign | Where-Object { $_.Title -eq $SiteDesignTitle }
+#             Write-Host "[INFO] Invoking Site Design $SiteDesignTitle using Add-SPOSiteDesignTask"
+#             Add-SPOSiteDesignTask -SiteDesignId $SiteDesign.Id.Guid -WebUrl $CreatedSiteUrl
+#             Start-Process "chrome.exe" $CreatedSiteUrl
+#         }
+#     }
+# }
+# Catch [Exception] {
+#     Write-Host $_.Exception.Message
+# }
 Disconnect-PnPOnline -Connection $CreatedSiteConnection
