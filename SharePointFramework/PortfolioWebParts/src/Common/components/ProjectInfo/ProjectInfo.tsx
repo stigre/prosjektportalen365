21ee80f2fb9614df53b381f12a8e33dc2d1fae2f
olemp <olemp@puzzlepart.com>
2019-02-13 12:45:43 +0100

    sp-entityportal-service 0.5.0

--- SharePointFramework/PortfolioWebParts/src/Common/components/ProjectInfo/ProjectInfo.tsx
+++ SharePointFramework/PortfolioWebParts/src/Common/components/ProjectInfo/ProjectInfo.tsx
@@ -72,14 +72,12 @@ export default class ProjectInfo extends React.Component<IProjectInfoProps, IPro
   private async fetchData() {
     try {
       const { pageContext } = this.props;
-      const { hubSiteId } = pageContext.legacyPageContext;
-      const groupId = this.props.showProjectInfo.RawObject.GtSiteId;
-      const hubSite = await HubSiteService.GetHubSiteById(pageContext.web.absoluteUrl, hubSiteId);
+      const hubSite = await HubSiteService.GetHubSiteById(pageContext.web.absoluteUrl, pageContext.legacyPageContext.hubSiteId);
       const spEntityPortalService = new SpEntityPortalService({ webUrl: hubSite.url, ...this.props.entity });
       const [entityItem, entityFields, editFormUrl] = await Promise.all([
-        spEntityPortalService.GetEntityItemFieldValues(groupId),
-        spEntityPortalService.GetEntityFields(),
-        spEntityPortalService.GetEntityEditFormUrl(groupId, pageContext.web.absoluteUrl),
+        spEntityPortalService.getEntityItemFieldValues(pageContext),
+        spEntityPortalService.getEntityFields(),
+        spEntityPortalService.getEntityEditFormUrl(pageContext, pageContext.web.absoluteUrl),
       ]);
       let properties = Object.keys(entityItem)
         .map(n => ({
