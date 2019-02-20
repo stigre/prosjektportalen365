76d6bd1ca6b6ddbb755ef563310444705e6cd8fe
olemp <olemp@puzzlepart.com>
2019-02-20 17:08:50 +0100

    Temp removal of document card preview image (looks bad with default group logos) #1

--- SharePointFramework/PortfolioWebParts/src/webparts/projectList/components/ProjectCard/ProjectCard.tsx
+++ SharePointFramework/PortfolioWebParts/src/webparts/projectList/components/ProjectCard/ProjectCard.tsx
@@ -2,23 +2,14 @@ import * as React from 'react';
 import styles from '../ProjectList.module.scss';
 import * as strings from 'ProjectListWebPartStrings';
 import IProjectCardProps from './IProjectCardProps';
-import { DocumentCard, DocumentCardPreview, DocumentCardTitle, DocumentCardLocation, DocumentCardActivity, DocumentCardActions, DocumentCardType, IDocumentCardPreviewImage } from "office-ui-fabric-react/lib/DocumentCard";
-import { ImageFit } from 'office-ui-fabric-react/lib/Image';
+import { DocumentCard, DocumentCardTitle, DocumentCardLocation, DocumentCardActivity, DocumentCardActions, DocumentCardType } from "office-ui-fabric-react/lib/DocumentCard";
 
-const ProjectCard = ({ project, onClickHref, showProjectInfo, fallbackPreviewImage }: IProjectCardProps): JSX.Element => {
-  const previewImage: IDocumentCardPreviewImage = {
-    previewImageSrc: project.Logo ? project.Logo : fallbackPreviewImage,
-    imageFit: ImageFit.cover,
-    height: 140,
-    width: 200
-  };
-
+export default ({ project, onClickHref, showProjectInfo }: IProjectCardProps): JSX.Element => {
   return (
     <DocumentCard
       className={styles.projectCard}
       type={DocumentCardType.normal}
       onClickHref={onClickHref}    >
-      <DocumentCardPreview previewImages={[previewImage]} />
       <DocumentCardTitle title={project.Title} shouldTruncate={false} />
       <DocumentCardLocation location={project.Phase || strings.NotSet} />
       <DocumentCardActivity
@@ -27,19 +18,7 @@ const ProjectCard = ({ project, onClickHref, showProjectInfo, fallbackPreviewIma
       <DocumentCardActivity
         activity={strings.ProjectManager}
         people={project.Manager ? [{ name: project.Manager.Title, profileImageSrc: getUserPhoto(project.Manager.Email) }] : []} />
-      <DocumentCardActions
-        actions={
-          [{
-            iconProps: { iconName: "AlignCenter" },
-            onClick: event => {
-              event.preventDefault();
-              event.stopPropagation();
-              showProjectInfo();
-            },
-          },
-          ]}
-        views={project.Views}
-      />
+      <DocumentCardActions actions={[{ iconProps: { iconName: "OpenInNewWindow" }, onClick: showProjectInfo }]} />
     </DocumentCard>
   );
 };
@@ -47,5 +26,3 @@ const ProjectCard = ({ project, onClickHref, showProjectInfo, fallbackPreviewIma
 function getUserPhoto(email: string, size = "L"): string {
   return `/_layouts/15/userphoto.aspx?size=${size}&accountname=${email}`;
 }
-
-export default ProjectCard;
